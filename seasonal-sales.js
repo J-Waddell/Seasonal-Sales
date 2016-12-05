97bd9e6e0ebcd190d6a94d254a64526553ac3164
J-Waddell <waddelljesse1@gmail.com>
2016-12-05 11:35:36 -0600

    var newRequest

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,1 +1,8 @@
-console.log('here i am!')
+console.log('here i am!');
+
+var newRequest = new XMLHttpRequest()
+
+newRequest.addEventListener ("load", showProducts)
+newRequest.open ("GET", products.json)
+newRequest.send ()
+
