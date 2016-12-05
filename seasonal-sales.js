d784e470608ec5605edf820545a5db6e6cdc36c5
J-Waddell <waddelljesse1@gmail.com>
2016-12-05 14:31:39 -0600

    get array working DUDE!!!

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -2,7 +2,17 @@ console.log('here i am!');
 
 var newRequest = new XMLHttpRequest()
 
+function showProducts (e) {
+        for (var i = products.length; i = 0; i++) {
+                products[i]
+        var length =
+        var data = JSON.parse (e.target.responseText)
+        console.log("data", data);
+        var products = data.products
+        console.log("products", products);
+        document.getElementById ('products').innerHTML = products[i].name
+}
+}
 newRequest.addEventListener ("load", showProducts)
-newRequest.open ("GET", products.json)
+newRequest.open ("GET", "products.json")
 newRequest.send ()
-
